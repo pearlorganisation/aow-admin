85afa97ef93679a08c7f590c550b9e3b1656f30c
Sachin3320 <saching@pearlorganisation.com>
2025-05-21 14:02:32 +0530

    minor fix

--- src/Pages/Vehicles/UnApprovedVehicle.jsx
+++ src/Pages/Vehicles/UnApprovedVehicle.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import VehicleTable from '../../Components/vehicles/vehicleTable';
 const UnApprovedVehicle = () => {
-  const data = [
-    { id: 1, name: 'John Doe', email: 'john@example.com', status: 'Not Approved', image: '/bike.png' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com', status: 'Not Approved', image: '/Car.jpg' },
-    { id: 3, name: 'Bob Johnson', email: 'bob@example.com', status: 'Not Approved', image: '/bike.png' },
-    { id: 4, name: 'John Doe', email: 'john@example.com', status: 'Not Approved', image: '/bike.png' },
-    { id: 5, name: 'Jane Smith', email: 'jane@example.com', status: 'Not Approved', image: '/Car.jpg' },
-    { id: 6, name: 'Bob Johnson', email: 'bob@example.com', status: 'Not Approved', image: '/bike.png' },
-    { id: 7, name: 'John Doe', email: 'john@example.com', status: 'Not Approved', image: '/bike.png' },
-    { id: 8, name: 'Jane Smith', email: 'jane@example.com', status: 'Not Approved', image: '/Car.jpg' },
-    { id: 9, name: 'Bob Johnson', email: 'bob@example.com', status: 'Not Approved', image: '/bike.png' },
-  ];
+    const data = [
+        { id: 1, name: 'John Doe', email: 'john@example.com', status: 'Not Approved', image: '/bike.png' },
+        { id: 2, name: 'Jane Smith', email: 'jane@example.com', status: 'Not Approved', image: '/Car.jpg' },
+        { id: 3, name: 'Bob Johnson', email: 'bob@example.com', status: 'Not Approved', image: '/bike.png' },
+        { id: 4, name: 'John Doe', email: 'john@example.com', status: 'Not Approved', image: '/bike.png' },
+        { id: 5, name: 'Jane Smith', email: 'jane@example.com', status: 'Not Approved', image: '/Car.jpg' },
+        { id: 6, name: 'Bob Johnson', email: 'bob@example.com', status: 'Not Approved', image: '/bike.png' },
+        { id: 7, name: 'John Doe', email: 'john@example.com', status: 'Not Approved', image: '/bike.png' },
+        { id: 8, name: 'Jane Smith', email: 'jane@example.com', status: 'Not Approved', image: '/Car.jpg' },
+        { id: 9, name: 'Bob Johnson', email: 'bob@example.com', status: 'Not Approved', image: '/bike.png' },
+    ];
 
-  return (
-    <div className="flex flex-col justify-center p-6">
-      <h1 className="text-2xl font-semibold mb-2">Vehicle Listing</h1>
-      <p className="mb-4 text-gray-600">Approve the vehicle if it's legitimate.</p>
-      <VehicleTable VehicleData={data} />
-    </div>
-  );
+    return (
+        <div className="flex flex-col justify-center p-6">
+            <h1 className="text-2xl font-semibold mb-2">Vehicle Listing</h1>
+            <p className="mb-4 text-gray-600">Approve the vehicle if it's legitimate.</p>
+            <VehicleTable VehicleData={data} />
+        </div>
+    );
 };
 
 export default UnApprovedVehicle;
